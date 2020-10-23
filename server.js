6e011de0bdb3c41b79a5fd5b6b860acc3eec002b
hoangminh122 <hoangminh12297@gmail.com>
2020-10-23 23:53:09 +0700

    update

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const favicon = require('express-favicon')
 const path = require('path')
 const app = express();
-
+app.use(favicon(__dirname+ '/build/favicon.ico'))   //add favicon for reactjs
 app.use(express.static(__dirname));
+
 app.use(express.static(path.join(__dirname,'build')));
-app.get('/*',(req,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'build','index.html'))
 });
 
