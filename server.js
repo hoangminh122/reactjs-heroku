98ecc083a5a7def0f02525b9c6fdbe9f1e085343
hoangminh122 <hoangminh12297@gmail.com>
2020-10-24 00:15:15 +0700

    sada

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const favicon = require('express-favicon')
-const path = require('path')
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname+ '/build/favicon.ico'))   //add favicon for reactjs
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(path.join(__dirname,'build')));
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'build','index.html'))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.listen(8080);
+app.get('/test', function (req, res) {
+    return res.send('test');
+   });
+app.listen(port);
